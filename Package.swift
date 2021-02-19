11c4db2f8a3a9632bedf8e8c2a7c4b259947d4fa
rathishubham7 <shubhamrathi@icloud.com>
2021-02-19 11:49:51 +0800

    updated package.swift

--- Package.swift
+++ Package.swift
@@ -6,21 +6,16 @@ import PackageDescription
 let package = Package(
     name: "TorusSwiftDirectSDK",
     products: [
-        // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
             name: "TorusSwiftDirectSDK",
             targets: ["TorusSwiftDirectSDK"]),
     ],
     dependencies: [
-        // Dependencies declare other packages that this package depends on.
         .package(name:"BestLogger", url: "https://github.com/rathishubham7/swift-logger", from: "0.0.1"),
-        .package(name: "TorusUtils", path: "../torus-utils-swift"),
-//        .package(name:"TorusUtils", url: "https://github.com/torusresearch/torus-utils-swift", .upToNextMajor(from: "0.0.1")),
+        .package(name:"TorusUtils", url: "https://github.com/torusresearch/torus-utils-swift", .upToNextMinor(from: "0.1.0")),
         .package(url: "https://github.com/krzyzanowskim/CryptoSwift.git", ._exactItem("1.3.2"))
     ],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "TorusSwiftDirectSDK",
             dependencies: ["TorusUtils", "BestLogger"],
