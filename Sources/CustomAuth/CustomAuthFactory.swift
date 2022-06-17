2643cf2eea1eac1b5ef0202b4c0c51d92253bc26
dhruv <dhruvjaiswal5@gmail.com>
2022-06-18 02:35:50 +0530

    custom auth sdk updated

--- Sources/CustomAuth/CustomAuthFactory.swift
+++ Sources/CustomAuth/CustomAuthFactory.swift
@@ -13,31 +13,31 @@ import TorusUtils
 /// A protocol should be implmented by users of `CustomAuth`. It provides a way
 /// to stub or mock the CustomAuth for testing.
 public protocol CASDKFactoryProtocol {
-    func createTorusUtils(nodePubKeys: Array<TorusNodePubModel>, loglevel: OSLogType, urlSession: URLSession,enableOneKey:Bool,network:EthereumNetworkFND) -> AbstractTorusUtils
+    func createTorusUtils(nodePubKeys: Array<TorusNodePubModel>, loglevel: OSLogType, urlSession: URLSession, enableOneKey: Bool, network: EthereumNetworkFND) -> AbstractTorusUtils
     func createFetchNodeDetails(network: EthereumNetworkFND, urlSession: URLSession) -> FetchNodeDetails
 }
 
 public class CASDKFactory: CASDKFactoryProtocol {
     public func createFetchNodeDetails(network: EthereumNetworkFND, urlSession: URLSession = URLSession.shared) -> FetchNodeDetails {
-        var proxyAddress:String = ""
-         switch network {
+        var proxyAddress: String = ""
+        switch network {
         case .MAINNET:
-           proxyAddress = FetchNodeDetails.proxyAddressMainnet
+            proxyAddress = FetchNodeDetails.proxyAddressMainnet
         case .ROPSTEN:
             proxyAddress = FetchNodeDetails.proxyAddressRopsten
         case .POLYGON:
             proxyAddress = FetchNodeDetails.proxyAddressPolygon
-        case .CUSTOM(let path):
+        case let .CUSTOM(path):
             return FetchNodeDetails(network: .CUSTOM(path: path))
         }
-        
+
         return FetchNodeDetails(proxyAddress: proxyAddress, network: network, urlSession: urlSession)
     }
 
-    public func createTorusUtils(nodePubKeys: Array<TorusNodePubModel> = [], loglevel: OSLogType, urlSession: URLSession = URLSession.shared,enableOneKey:Bool,network:EthereumNetworkFND) -> AbstractTorusUtils {
+    public func createTorusUtils(nodePubKeys: Array<TorusNodePubModel> = [], loglevel: OSLogType, urlSession: URLSession = URLSession.shared, enableOneKey: Bool, network: EthereumNetworkFND) -> AbstractTorusUtils {
         let allowHost = network.signerMap.appending("/api/allow")
         let signerHost = network.signerMap.appending("/api/sign")
-        return TorusUtils(nodePubKeys: nodePubKeys, loglevel: loglevel, urlSession: urlSession,enableOneKey: enableOneKey,signerHost: signerHost,allowHost: allowHost)
+        return TorusUtils(nodePubKeys: nodePubKeys, loglevel: loglevel, urlSession: urlSession, enableOneKey: enableOneKey, signerHost: signerHost, allowHost: allowHost,network: network)
     }
 
     public init() {
