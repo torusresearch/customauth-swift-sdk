3f98182c35616c51465ccd5aacf4c9214d2d6ffa
dhruv <dhruv@tor.us>
2022-11-07 11:13:30 +0530

    ASWebAuthentication deallocation fix

--- Sources/CustomAuth/Helpers/ASWebAuthSession.swift
+++ Sources/CustomAuth/Helpers/ASWebAuthSession.swift
@@ -8,16 +8,17 @@
 import AuthenticationServices
 import Foundation
 
-@available(iOS 13.0, *)
 open class ASWebAuthSession: NSObject, TorusURLHandlerTypes {
     var redirectURL: URL?
+    // Ensure that there is a strong reference to the SFAuthenticationSession instance when the session is in progress.
+    private var authSession: ASWebAuthenticationSession?
     public init(redirectURL: String) {
         self.redirectURL = URL(string: redirectURL)
     }
 
     public func handle(_ url: URL, modalPresentationStyle: UIModalPresentationStyle) {
         let redirectURLScheme = redirectURL?.scheme ?? CustomAuth.didHandleCallbackURL.rawValue
-        let authSession = ASWebAuthenticationSession(
+        authSession = ASWebAuthenticationSession(
             url: url, callbackURLScheme: redirectURLScheme) { callbackURL, authError in
                 guard
                     authError == nil,
@@ -28,12 +29,11 @@ open class ASWebAuthSession: NSObject, TorusURLHandlerTypes {
                 }
                 CustomAuth.handle(url: callbackURL)
             }
-        authSession.presentationContextProvider = self
-        authSession.start()
+        authSession?.presentationContextProvider = self
+        authSession?.start()
     }
 }
 
-@available(iOS 13.0, *)
 extension ASWebAuthSession: ASWebAuthenticationPresentationContextProviding {
     public func presentationAnchor(for session: ASWebAuthenticationSession) -> ASPresentationAnchor {
         let window = UIApplication.shared.windows.first { $0.isKeyWindow }
