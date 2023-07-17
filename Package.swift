da841b79dfc922232cde64a8c3b60edf392f895b
chaitanyapotti <chaitanya.potti@gmail.com>
2023-07-17 22:21:41 +0800

    Add macos version

--- Package.swift
+++ Package.swift
@@ -6,7 +6,7 @@ import PackageDescription
 let package = Package(
     name: "CustomAuth",
     platforms: [
-        .iOS(.v13)
+        .iOS(.v13), .macOS(.v10_15)
     ],
     products: [
         .library(
