23882249d8edac37b03aa19d44cdab2983f9d54e
metalurgical <97008724+metalurgical@users.noreply.github.com>
2024-10-14 05:31:26 +0200

    feat: remove celeste support
    additionally update minimum swift version

--- Package.swift
+++ Package.swift
@@ -6,7 +6,7 @@ import PackageDescription
 let package = Package(
     name: "CustomAuth",
     platforms: [
-        .iOS(.v13), .macOS(.v11)
+        .iOS(.v14), .macOS(.v11)
     ],
     products: [
         .library(
@@ -14,7 +14,7 @@ let package = Package(
             targets: ["CustomAuth"])
     ],
     dependencies: [
-        .package(url: "https://github.com/torusresearch/torus-utils-swift.git", branch: "encapsulate_torusnetwork"),
+        .package(url: "https://github.com/torusresearch/torus-utils-swift.git", branch: "remove_celeste_support"),
         .package(url: "https://github.com/auth0/JWTDecode.swift.git", from: "3.1.0"),
         // NB: jwt-kit may only be a test dependency or it will break cocoapods support
         .package(url: "https://github.com/vapor/jwt-kit.git", from: "4.13.0"),
